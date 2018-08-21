0c7a26c0675c9a01db0ed828569a79cec913bd8b
LUTlp <lutlp@outlook.com>
2018-08-21 19:50:02 +0200

    défilement auto

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,9 +53,17 @@ function showSlides(n) {
 }
 
 
-$('.autoplay').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
